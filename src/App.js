12dfcced72c62ab4285a7319fc48455222954a22
SamuliKai <127877375+SamuliKai@users.noreply.github.com>
2023-11-02 19:20:52 +0200

    axios lisäys

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
+import axios from 'axios';
 
 function App() {
+  const [sek, setSek] = useState(0)
+  const [eur, setEur] = useState(0)
+  
+  function Calculate (e) {
+    e.preventDefault()
+    const result = sek * 0.085
+    setEur(result)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div id="container">
+      <h3>Valuutta muunnin: Kruunut Euroiksi</h3>
+      <form onSubmit={Calculate}>
+        <div>
+          <label>SEK</label>
+          <input type="number" value={sek} onChange={e => setSek(e.target.value)}/>
+        </div>
+        <div>
+          <label>EUR</label>
+          <output>{eur.toFixed(2)}</output>
+        </div>
+        <button>Calculate</button>
+      </form>
     </div>
   );
 }
